e1b46a6f3658b25451bf08f7f839569df98b0739
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-05-22 13:27:45 +0600

    placement

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 
-app.use(cors())
-app.use(express.json())
-
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('hello from the other side')
