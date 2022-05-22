b6eeb54c77774c1c1524a9bce789b5c5af8d7113
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-05-22 13:23:51 +0600

    heroku

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 
+app.use(cors())
+app.use(express.json())
+
 const app = express()
 const port = process.env.PORT || 5000
 
