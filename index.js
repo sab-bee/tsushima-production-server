aef6272ee9dafb32e4f8b42e3120043db14e8071
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-05-22 23:23:28 +0600

    order post function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => console.log('listening to port', port))
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bmzoc.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -41,7 +41,11 @@ async function run() {
     await client.connect()
     console.log('db connected')
     const partsCollection = client.db('tsushimaCorporation').collection('parts')
+    const orderCollection = client
+      .db('tsushimaCorporation')
+      .collection('orders')
 
+    // generate jwt after user logged in
     app.get('/account/:email', async (req, res) => {
       const email = req.params
       const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
@@ -50,9 +54,26 @@ async function run() {
       res.send({ token })
     })
 
+    //all the parts public api
     app.get('/parts', async (req, res) => {
       const query = req.query
       const result = await partsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // get individual product
+    app.get('/part/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await partsCollection.findOne(query)
+      res.send(result)
+    })
+
+    // post order
+    app.post('/item', verifyJWT, async (req, res) => {
+      const item = req.body
+      console.log(item)
+      const result = await orderCollection.insertOne(item)
       res.send(result)
     })
   } finally {
